678a86343c6dd829352d7ada5a45f7c7045afdac
ricksanchez <ricksanchez@localhost.localdomain>
2020-01-19 22:31:10 -0600

    Add pingsweep.sh

---
+++ pingsweep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#a simple ip sweep
+#parameter one is the start of range, 2 is the end of the range
+
+network=$(echo $1| cut -d '.' -f1,2,3)
+host=$(echo $1| cut -d '.' -f4)
+endHost=$(echo $2| cut -d '.' -f4)
+
+until [ $host -gt $endHost ]; do
+	ip=$network.$host
+	(ping -c 1 $ip| grep "bytes from"| cut -d " " -f4| cut -d ":" -f1)&
+	#echo $ip
+	let "host++"
+	#echo $host
+done
+wait
