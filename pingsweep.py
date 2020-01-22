9b5ad8ed2bd29c2df7b43083d09cc16bb9c8227a
ricksanchez <ricksanchez@localhost.localdomain>
2020-01-21 20:11:52 -0600

    Ping sweep works, but is slow as hell.  Tried figuring out multiprocessing, but need to revist functions.  Will work on that later.

--- pingsweep.py
+++ pingsweep.py
@@ -2,12 +2,14 @@
 
 import os
 
-print("Let's throw some icmp around. You can't do more than a `/24.")
+print "Let's throw some icmp around. You can't do more than a `/24."
 rangeStart = raw_input("Which ip should we start with? ")
 subnet, startIp = rangeStart.rsplit(".",1)
 rangeEnd = raw_input("Which ip should we stop at? ")
 subnet, endIp = rangeEnd.rsplit(".",1)
 
-for x in range (startIp, endIp)
-	print(x)
-
+for x in range(int(startIp), (int(endIp)+1)):
+	ip = (subnet+"."+str(x))
+	response = os.system("ping -c 1 " + ip + " > /dev/null 2>&1")
+	if response == 0:
+		print ip, "responded."
